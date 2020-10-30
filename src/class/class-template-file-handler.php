bbb89777403d51cdf837871894ba7eb6294a0569
Varun <varunsridharan23@gmail.com>
2020-10-30 18:27:12 +0530

    Updated

--- src/class/class-template-file-handler.php
+++ src/class/class-template-file-handler.php
@@ -21,13 +21,6 @@ class Template_File_Handler extends File_Handler {
 		parent::__construct( $src, false );
 		$this->parent_file = ( ! empty( $parent_file ) ) ? dirname( $parent_file ) . '/' : false;
 		$this->src         = $this->extract_src_details();
-		gh_log( array(
-			'WORK_DIR'         => WORK_DIR,
-			'GLOBAL_REPO_PATH' => GLOBAL_REPO_PATH,
-			'raw_src'          => $src,
-			'new_src'          => $this->src,
-			'parent_file'      => $parent_file,
-		) );
 	}
 
 	/**
