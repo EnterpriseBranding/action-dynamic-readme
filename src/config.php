f565d2b25a129becb1a9f555386affc0a6871d47
Varun <varunsridharan23@gmail.com>
2020-10-30 18:06:47 +0530

    Fixed A Stupid Issue.

--- src/config.php
+++ src/config.php
@@ -6,7 +6,7 @@ $delimeter   = ( ! empty( $delimeter ) ) ? $delimeter : '${{ }}';
 $global_path = false;
 
 define( 'APP_PATH', __DIR__ . '/' );
-define( 'WORK_DIR', $GITHUB_WORKSPACE );
+define( 'WORK_DIR', gh_env( 'GITHUB_WORKSPACE' ) );
 define( 'TEMPLATE_REPO_PATH', '/dynamic-readme-tmp/repos/' );
 define( 'TEMPLATE_ENGINE', gh_input( 'TEMPLATE_ENGINE', '${{ }}' ) );
 define( 'TEMPLATE_DELIMITER', $delimeter );
